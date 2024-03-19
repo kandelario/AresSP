ce2b5b0478088e4e675bdbdc50f12e524526bde1
Kande Pacheco <cande.pacheco@gmail.com>
2024-03-19 13:21:21 -0600

    Acomodo de formulario para inventario.

--- resources/views/inventarios/fields.blade.php
+++ resources/views/inventarios/fields.blade.php
@@ -8,10 +8,10 @@
 <div class="form-group col-sm-6">
     {!! Form::label('existencia', 'Existencia:') !!}
     {!! Form::number('existencia', 1, ['class' => 'form-control', 'required']) !!}
-</div>existence
+</div>
 
 <!-- password Field -->
-<div class="form-group col-sm-3">
+<div class="form-group col-sm-6">
     {!! Form::label('image', 'Imagen:') !!}
     {!! Form::file('image', ['class' => 'form-control file', 'accept' => 'image/*']) !!}
 </div>
