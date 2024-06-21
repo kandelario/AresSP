af11c2e4de50de3f597948c43640c1b3d12917d2
kandelario <cande.pacheco@gmail.com>
2024-06-21 17:41:03 -0600

    creación de AdminController con recursos. modificación de ruta de AdminController en archivo de rutas admin.php.

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\InventarioController;
