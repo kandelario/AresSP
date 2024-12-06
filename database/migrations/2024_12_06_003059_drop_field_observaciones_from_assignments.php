51dc5ef5742378761e85818ffe14fec0250172e9
kandelario <cande.pacheco@gmail.com>
2024-12-05 18:40:26 -0600

    se quita metodo down de la migración para eliminar el campo observaciones.

--- database/migrations/2024_12_06_003059_drop_field_observaciones_from_assignments.php
+++ database/migrations/2024_12_06_003059_drop_field_observaciones_from_assignments.php
@@ -16,13 +16,4 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('assignments', function (Blueprint $table) {
-            //
-        });
-    }
 };
